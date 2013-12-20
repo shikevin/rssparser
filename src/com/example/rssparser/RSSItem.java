a08e23a629c27cffd5fcc6ea4fca950fab93992d
Kevin Shi <kevin@dare2tee.com>
2013-12-20 11:36:35 -0500

    added layout file in res, and defined RSSItem class in src

---
+++ src/com/example/rssparser/RSSItem.java
@@ -0,0 +1,23 @@
+package com.example.rssparser;
+public class RSSItem {
+// item title
+private String title;
+// item link
+private String link;
+public String getTitle() {
+return title;
+}
+public void setTitle(String title) {
+this.title = title;
+}
+public String getLink() {
+return link;
+}
+public void setLink(String link) {
+this.link = link;
+}
+@Override
+public String toString() {
+return title;
+}
+}
